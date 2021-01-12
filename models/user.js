086a460d476785819ae52dc68e266edf382ffcab
Dario <darioandrade@email.arizona.edu>
2021-01-12 13:21:47 -0700

    Added User Schema

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {type: String, require: true},
+    email: {type: String, require: true, unique: true}, //creates index, which speeds up searching and quering
+    password : {type: String, require: true, minLenth: 6},
+    image: {type: String, require: true},
+    places: {type: String, require: true}, 
+
+});
+
+userSchema.plugin(uniqueValidator); //amkes sure we have only one user with one email
+
+module.exports = mongoose.model('User', userSchema);    //first string defines the name of the collection
